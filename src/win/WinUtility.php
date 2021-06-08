8e4b4f03dd0457e56f92be425544ed8f15a57614
Pansystar <wh.bt3.p2@gmail.com>
2021-06-08 11:16:19 +0900

    コメントを追加

--- src/win/WinUtility.php
+++ src/win/WinUtility.php
@@ -7,6 +7,7 @@
     class WinUtility {
         /**
          * @brief 一時ファイル用のファイル名を取得する
+         * @todo ランダムなファイル名になるように処理を変更する
          */
         public static function GetTempFilePath() {
             return getenv("TEMP") . "\\temp.cookies";
